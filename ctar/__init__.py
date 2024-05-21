116c0aeff780a2f69aed4fa1402906e0b0335046
anatori <53577980+anatori@users.noreply.github.com>
2024-05-21 15:52:44 -0400

    added plot to import

--- ctar/__init__.py
+++ ctar/__init__.py
@@ -1,8 +1,8 @@
-from . import simu, method, data_loader, util
+from . import simu, method, plot, data_loader, util
 
 # expose functions so that scdrs.score_cell, scdrs.preprocess can be called
 # from .method import score_cell
 # from .pp import preprocess
 from .version import __version__,__version_info__
 
-__all__ = ["simu", "method", "data_loader", "util"]
+__all__ = ["simu", "method", "plot", "data_loader", "util"]
