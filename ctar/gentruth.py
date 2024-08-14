8a18a3b32470c9af83ba6d98b56752a16d264529
Anatori Serena Prieto <asprieto@lanec1.compbio.cs.cmu.edu>
2024-08-14 18:26:47 -0400

    implement odds ratio

--- ctar/gentruth.py
+++ ctar/gentruth.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import numpy as np
+import scipy as sp
+from statsmodels import stats
 import pybedtools
 from pybedtools import BedTool
 import os
+import matplotlib.pyplot as plt
+import matplotlib_venn as venn
 
 
 def preprocess_df(df):
@@ -23,7 +27,7 @@ def preprocess_df(df):
 def intersect_beds(bed1,bed2,deduplicate=True,col_names = ['bed1_chr','bed1_start','bed1_end','bed1_id','bed2_chr','bed2_start','bed2_end','bed2_id']):
     ''' BedTools intersection from two BedTool objects and returns a DataFrame.
     '''
-    # intersect query bed with ref bed
+
     intersect_df = bed1.intersect(bed2,wa=True,wb=True).to_dataframe()
     intersect_df.columns = col_names
     if deduplicate:
@@ -31,14 +35,15 @@ def intersect_beds(bed1,bed2,deduplicate=True,col_names = ['bed1_chr','bed1_star
     return intersect_df
 
 
-def summarize_df(query_file, reference_path, storage_path):
+def summarize_df(query_item, reference_path, storage_path):
 
     ''' Summarizes existing tsv files to obtain marginal and joint n_genes, n_snps, n_links.
     
     Parameters
     -------
-    query_file : str 
-        Path to file containing query dataset. Must be in .tsv.gz format.
+    query_item : str or pd.DataFrame
+        Path to file containing query dataset in .tsv.gz format OR dataframe containing
+        query data. Should have columns ['chr','start','gene'].
 
     reference_path : str
         Path to all other files to intersect query_file with. Must be in .tsv.gz format.
@@ -56,7 +61,11 @@ def summarize_df(query_file, reference_path, storage_path):
     '''
     
     # load query df
-    query_df = pd.read_csv(query_file, sep='\t', compression='gzip',index_col=0)
+    if isinstance(x, pd.DataFrame):
+        query_df = query_item
+    else:
+        query_df = pd.read_csv(query_file, sep='\t', compression='gzip',index_col=0)
+
     query_df = preprocess_df(query_df)
     querybed = BedTool.from_dataframe(query_df[['chr','start','end','gene']])
 
@@ -101,11 +110,97 @@ def summarize_df(query_file, reference_path, storage_path):
 
     if '_pos' not in query_label:
         zipped = zip([gene_dic,snp_dic,links_dic],['gene.tsv','snp.tsv','links.tsv'])
-    else: zipped = zip(links_dic,'links.tsv')
+    else: zipped = zip([links_dic],['links.tsv'])
 
     for dic, filename in zipped:
         df = pd.read_csv(storage_path + filename, sep='\t', index_col=0)
         df[query_label] = pd.Series(dic) # making into series will ensure that indices match, even if they're out of order
         df.loc[query_label] = pd.Series(dic)
         df.to_csv(os.path.join(storage_path, filename), sep='\t')
+
+
+def plot_venn2(df,labels,pos=False):
+
+    ''' Display 2 venn diagrams on intersecting (1) all links and (2) positive-labelled links.
+    
+    Parameters
+    -------
+    df : pd.DataFrame
+        pd.DataFrame with labels in columns.
+    labels : list
+        List of strings
+
+    Returns
+    -------
+    
+    '''
+
+    # all labels
+    n3 = df.loc[labels[0],labels[1]] # intersection
+    n2 = df.loc[labels[0],labels[0]] - n3 # label 0
+    n4 = df.loc[labels[1],labels[1]] - n3 # label 1
+
+    venn1 = venn.venn2([n2, n4, n3],set_labels=labels)
+    plt.title('All links')
+    plt.show()
+
+    # positive only
+    n3 = df.loc[labels[0]+'_pos',labels[1]+'_pos'] # intersection
+    n2 = df.loc[labels[0]+'_pos',labels[1]] - n3 # pos label 0 in label 1 univ
+    n4 = df.loc[labels[0],labels[1]+'_pos'] - n3 # pos label 1 in label 0 univ
+
+    venn1 = venn.venn2([n2, n4, n3],set_labels=labels)
+
+    plt.title('Positive links')
+    plt.show()
+
+
+def paired_stats_df(df_dic,labels):
+    ''' Fetches marginal and joint stats for pair of labels.
+    '''
+
+    stats_df = pd.DataFrame(columns=labels + ['joint'],index=df_dic.keys(),data=0)
+
+    for cat in df_dic.keys():
+        stats_df.loc[cat,labels[0]] = df_dic[cat].loc[labels[0],labels[0]]
+        stats_df.loc[cat,labels[1]] = df_dic[cat].loc[labels[1],labels[1]]
+        stats_df.loc[cat,'joint'] = df_dic[cat].loc[labels[0],labels[1]]
+
+    return stats_df
+
+
+def odds_ratio_poslinks(df,ha_correction=True):
+    ''' Provides odds ratio and chi^2 p-value with (+0.5) correction 
+    for positive links.
+
+    '''
+    
+    labels = [x for x in df.columns  if '_pos' not in x]
+    odds_df = pd.DataFrame(index=labels,columns=labels,data=0.0)
+    pval_df = pd.DataFrame(index=labels,columns=labels,data=0.0)
+    contig_df = pd.DataFrame(index=labels,columns=labels,data=[])
+    label_tup = [(a, b) for idx, a in enumerate(labels) for b in labels[idx + 1:]]
+    
+    for label_1, label_2 in label_tup:
+        
+        n1 = df.loc[label_1,label_2] # universe
+        n3 = df.loc[label_1+'_pos',label_2+'_pos'] # intersection
+        n2 = df.loc[label_1+'_pos',label_2] - n3 # pos label 0 in label 1 univ
+        n4 = df.loc[label_1,label_2+'_pos'] - n3 # pos label 1 in label 0 univ
+        
+        contigency_mat = np.array([[n3, n4], [n2, n1]]) 
+        odds_ratio = stats.contingency_tables.Table2x2(contigency_mat,shift_zeros=ha_correction).oddsratio # +0.5 haldane-anscombe / yates correction
+        pval = sp.stats.chi2_contingency(contigency_mat).pvalue
+        
+        odds_df.loc[label_1,label_2] = odds_ratio
+        odds_df.loc[label_2,label_1] = odds_ratio
+        
+        pval_df.loc[label_1,label_2] = pval
+        pval_df.loc[label_2,label_1] = pval
+
+        contig_df.loc[label_1,label_2] = contigency_mat
+        contig_df.loc[label_2,label_1] = contigency_mat
+        
+    return {'odds_ratio':odds_df,'pvals':pval_df,'contingency':contig_df}
+
     
