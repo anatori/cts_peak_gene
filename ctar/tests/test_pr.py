a4ccc5c6305aaacc022ebd1605b2242c0002e7b4
Anatori Serena Prieto <asprieto@lanec1.compbio.cs.cmu.edu>
2025-05-28 10:51:43 -0400

    unit testing

---
+++ ctar/tests/test_pr.py
@@ -0,0 +1,40 @@
+import pytest
+import numpy as np
+import ctar
+
+
+def load_toy_data():
+    ''' Generate toy data from poisson distribution.
+    '''
+    mat_x = np.random.poisson(size=(50,100))
+    mat_y = np.random.poisson(size=(50,100))
+    return mat_x, mat_y
+
+
+def test_vectorized_poisson_regression():
+    ''' Test vectorized PR.
+    '''
+
+    mat_x, mat_y = load_toy_data()
+    v_beta0, v_beta1 = ctar.method.vectorized_poisson_regression_final(mat_x, mat_y, tol=1e-3)
+    
+    v_beta0_true = np.zeros((50,))
+    v_beta1_true = np.zeros((50,))
+    for k in range(mat_x.shape[1]):
+        v_beta0_true[k], v_beta1_true[k] = ctar.method.fit_poisson(mat_x[:,k], mat_y[:,k], return_both=True)
+
+    # v_beta0
+    err_msg = (
+        "avg_abs_beta0_mean_dif=%0.2e"
+        % (np.mean(np.abs(v_beta0_true - v_beta0)))
+    )
+    assert np.allclose(v_beta0, v_beta0_true, rtol=1e-4, equal_nan=True), err_msg
+
+    # v_beta1
+    err_msg = (
+        "avg_abs_beta1_mean_dif=%0.2e"
+        % (np.mean(np.abs(v_beta1_true - v_beta1)))
+    )
+    assert np.allclose(v_beta1, v_beta1_true, rtol=1e-4, equal_nan=True), err_msg
+
+    return
