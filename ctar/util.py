4aec3e9563638eaa682c7b9d55326fba3c346a83
Anatori Serena Prieto <asprieto@lanec1.compbio.cs.cmu.edu>
2025-05-30 13:44:28 -0400

    unit test for pr

--- ctar/util.py
+++ ctar/util.py
@@ -3,3 +3,13 @@ import ctar
 
 def print_info():
     print("ctar version: %s" % ctar.__version__)
+
+def get_cli_head():
+    MASTHEAD = "******************************************************************************\n"
+    MASTHEAD += "* CTAR cell type-specific ATAC and RNA linking\n"
+    MASTHEAD += "* Version %s\n" % ctar.__version__
+    MASTHEAD += "* Ana Prieto\n"
+    MASTHEAD += "* CMU CBD\n"
+    MASTHEAD += "* MIT License\n"
+    MASTHEAD += "******************************************************************************\n"
+    return MASTHEAD
