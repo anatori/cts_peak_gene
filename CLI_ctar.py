eb782296edc410fc0081bdc942132ae9c2a0026b
Anatori Serena Prieto <asprieto@lanec1.compbio.cs.cmu.edu>
2025-06-25 15:12:55 -0400

    add pr option

--- CLI_ctar.py
+++ CLI_ctar.py
@@ -25,6 +25,10 @@ create_ctrl : create control peaks or peak-gene pairs
 compute_corr : compute pearson correlation for control pairs
     - Input : --job | --multiome_file | --batch_size | --batch | --links_file | --target_path | --bin_config | --bin_type
     - Output : a folder containing several arrays of control peak-gene pair correlations of shape (n_ctrl,) or (n_batch, n_ctrl)
+
+compute_pr : compute poisson regression coeff for control pairs
+    - Input : --job | --multiome_file | --batch_size | --batch | --links_file | --target_path | --bin_config | --bin_type
+    - Output : a folder containing several arrays of control peak-gene pair coefficients of shape (n_ctrl,) or (n_batch, n_ctrl)
     
 TODO
 ----
@@ -51,6 +55,7 @@ def main(args):
     BIN_CONFIG = args.binning_config
     BIN_TYPE = args.binning_type
     PYBEDTOOLS_PATH = args.pybedtools_path
+    METHOD = args.method
 
     BATCH = BATCH_ID + BATCH_OFFSET
 
@@ -71,11 +76,10 @@ def main(args):
         assert LINKS_FILE is not None, "--links_file required for --job=%s" % JOB
         assert TARGET_PATH is not None, "--target_path required for --job=%s" % JOB
     if JOB in ["create_ctrl"]:
-        assert BIN_CONFIG is not None, (
-            "--binning_config required for --job=%s" % JOB
-        )
+        assert BIN_CONFIG is not None, "--binning_config required for --job=%s" % JOB
+        assert METHOD is not None, "--method required for --job=%s" % JOB
     if JOB in ["compute_corr"]:
-        assert BATCH is not None, "--batch required for --job=%s" % JOB
+        assert BATCH_ID is not None, "--batch_id required for --job=%s" % JOB
 
     # Print input options
     header = ctar.util.get_cli_head()
@@ -92,6 +96,7 @@ def main(args):
     header += "--binning_config %s\\\n" % BIN_CONFIG
     header += "--binning_type %s\\\n" % BIN_TYPE
     header += "--pybedtools_path %s\\\n" % PYBEDTOOLS_PATH
+    header += "--method %s\\\n" % METHOD
     print(header)
 
     ###########################################################################################
@@ -102,6 +107,7 @@ def main(args):
     if JOB in [
         "create_ctrl",
         "compute_corr",
+        "compute_pr"
     ]:
         print("# Loading --multiome_file")
         if MULTIOME_FILE.endswith('.h5mu'):
@@ -134,8 +140,12 @@ def main(args):
         n_atac_mean, n_atac_gc, n_rna_mean, n_rna_var, n_ctrl = BIN_CONFIG.split('.')
         n_atac_mean, n_atac_gc, n_ctrl = map(int, [n_atac_mean, n_atac_gc, n_ctrl])
 
-        # Setting corr destination path
-        corr_path = os.path.join(TARGET_PATH, 'ctrl_corr')
+        if METHOD == 'pr':
+            # Setting corr destination path
+            corr_path = os.path.join(TARGET_PATH, 'ctrl_poiss')
+        if METHOD == 'corr':
+            # Setting corr destination path
+            corr_path = os.path.join(TARGET_PATH, 'ctrl_corr')
         # Setting peak source path
         ctrl_path = os.path.join(TARGET_PATH, 'ctrl_peaks')
 
@@ -199,11 +209,12 @@ def main(args):
             eval_df[f'combined_bin_{BIN_CONFIG}'] = eval_df[f'atac_{atac_type}_bin_{n_atac_mean}.{n_atac_gc}'].astype(str) + '_' + eval_df[f'rna_mean_var_bin_{n_rna_mean}.{n_rna_var}'].astype(str)
             eval_df.to_csv(LINKS_FILE, sep=sep)
 
-            unique_bins =  eval_df[f'combined_bin_{BIN_CONFIG}'].unique()
-            if len(os.listdir(final_ctrl_path)) < len(unique_bins):
+            existing_corr = os.listdir(final_ctrl_path)
+            if len(existing_corr) < len(ctrl_dic.keys()):
                 for key in ctrl_dic:
-                    if key in unique_bins:
-                        np.save(os.path.join(final_ctrl_path, f'ctrl_{key}.npy'), ctrl_dic[key])
+                    filename = f'ctrl_{key}.npy'
+                    if filename not in existing_corr:
+                        np.save(os.path.join(final_ctrl_path, filename), ctrl_dic[key])
             else:
                 print(f"# {final_ctrl_path} is already complete.")
             
@@ -250,20 +261,112 @@ def main(args):
         else:
 
             ctrl_files = os.listdir(os.path.join(ctrl_path, f'ctrl_links_{BIN_CONFIG}'))
-            if len(ctrl_files) < end: end = len(ctrl_files)
+            n_ctrl_files = len(ctrl_files)
+            if n_ctrl_files < end: end = n_ctrl_files
             ctrl_files = ctrl_files[start:end]
 
             for ctrl_file in ctrl_files:
                 ctrl_links = np.load(os.path.join(ctrl_path, f'ctrl_links_{BIN_CONFIG}', ctrl_file))[:, :n_ctrl]
 
-                atac_data = adata_atac[:, ctrl_links[:, 0]].layers['counts']
-                rna_data = adata_rna[:, ctrl_links[:, 1]].layers['counts']
+                atac_data = adata_atac.layers['counts'][:, ctrl_links[:, 0]]
+                rna_data = adata_rna.layers['counts'][:, ctrl_links[:, 1]]
                 ctrl_pearsonr = ctar.method.pearson_corr_sparse(atac_data, rna_data)
                 
                 print(f"# Saving pearsonr_{os.path.basename(ctrl_file)} to {final_corr_path}")
                 np.save(os.path.join(final_corr_path, f'pearsonr_{os.path.basename(ctrl_file)}'), np.array(ctrl_pearsonr))
 
+            # check for missing files
+            if end == n_ctrl_files:
+                print('# Last array job...')
 
+                final_ctrl_path = os.path.join(ctrl_path, f'ctrl_links_{BIN_CONFIG}')
+                missing_bins = ctar.data_loader.check_missing_bins(final_ctrl_path, final_corr_path)
+
+                if len(missing_bins) > 0:
+                    print(f"# Computing correlation for an additional {len(missing_bins)} ctrls.")
+
+                for ctrl_file in missing_bins:
+                    ctrl_links = np.load(os.path.join(ctrl_path, f'ctrl_links_{BIN_CONFIG}', ctrl_file))[:, :n_ctrl]
+
+                    atac_data = adata_atac.layers['counts'][:, ctrl_links[:, 0]]
+                    rna_data = adata_rna.layers['counts'][:, ctrl_links[:, 1]]
+                    ctrl_pearsonr = ctar.method.pearson_corr_sparse(atac_data, rna_data)
+                    
+                    print(f"# Saving pearsonr_{os.path.basename(ctrl_file)} to {final_corr_path}")
+                    np.save(os.path.join(final_corr_path, f'pearsonr_{os.path.basename(ctrl_file)}'), np.array(ctrl_pearsonr))
+
+    if JOB == "compute_pr":
+        print("# Running --job compute_pr")
+
+        start, end = BATCH * BATCH_SIZE, (BATCH + 1) * BATCH_SIZE
+        final_corr_path = os.path.join(corr_path, f'ctrl_poiss_{BIN_CONFIG}')
+
+        if os.path.exists(final_corr_path) is False:
+            os.makedirs(final_corr_path)
+
+        if f'{n_rna_mean}.{n_rna_var}' == 'inf.inf':
+
+            ctrl_peaks = np.load(os.path.join(ctrl_path, f'ctrl_peaks_{BIN_CONFIG}.npy'))
+            adata_atac.varm['ctrl_peaks'] = ctrl_peaks[:, :n_ctrl]
+
+            last_batch = eval_df.shape[0] // BATCH_SIZE
+            if BATCH == last_batch: end = eval_df.shape[0]
+
+            links = links_arr[:, start:end]
+            ctrl_peaks = adata_atac[:, links[0]].varm['ctrl_peaks']
+            rna_data = adata_rna[:, links[1]].layers['counts']
+
+            ctrl_poissonb = []
+            for i in range(end - start):
+                beta0, beta1 = ctar.method.vectorized_poisson_regression_final(adata_atac[:, ctrl_peaks[i]].layers['counts'], rna_data[:, [i]])
+                ctrl_poissonb.append(beta1.flatten())
+
+            if len(ctrl_poissonb) > 0:
+                print(f"# Saving poissonb_ctrl_{start}_{end}.npy to {final_corr_path}")
+                np.save(os.path.join(final_corr_path, f'poissonb_ctrl_{start}_{end}.npy'), ctrl_poissonb)
+
+            curr_n_files = len(os.listdir(final_corr_path))
+            if curr_n_files == (last_batch + 1):
+                print(f"# All {curr_n_files} batches complete. Consolidating null")
+                full_ctrl_poissonb = ctar.data_loader.consolidate_null(final_corr_path + '/', startswith = 'poissonb_ctrl_', b=last_batch+1)
+                np.save(os.path.join(corr_path, f'ctrl_poiss_{BIN_CONFIG}.npy'), full_ctrl_poissonb)
+
+        else:
+
+            ctrl_files = os.listdir(os.path.join(ctrl_path, f'ctrl_links_{BIN_CONFIG}'))
+            n_ctrl_files = len(ctrl_files)
+            if n_ctrl_files < end: end = n_ctrl_files
+            ctrl_files = ctrl_files[start:end]
+
+            for ctrl_file in ctrl_files:
+                ctrl_links = np.load(os.path.join(ctrl_path, f'ctrl_links_{BIN_CONFIG}', ctrl_file))[:, :n_ctrl]
+
+                atac_data = adata_atac.layers['counts'][:, ctrl_links[:, 0]]
+                rna_data = adata_rna.layers['counts'][:, ctrl_links[:, 1]]
+                _, ctrl_poissonb = ctar.method.vectorized_poisson_regression_final(atac_data, rna_data)
+                
+                print(f"# Saving poissonb_{os.path.basename(ctrl_file)} to {final_corr_path}")
+                np.save(os.path.join(final_corr_path, f'poissonb_{os.path.basename(ctrl_file)}'), ctrl_poissonb.flatten())
+
+            # check for missing files
+            if end == n_ctrl_files:
+                print('# Last array job...')
+
+                final_ctrl_path = os.path.join(ctrl_path, f'ctrl_links_{BIN_CONFIG}')
+                missing_bins = ctar.data_loader.check_missing_bins(final_ctrl_path, final_corr_path)
+
+                if len(missing_bins) > 0:
+                    print(f"# Computing correlation for an additional {len(missing_bins)} ctrls.")
+
+                for ctrl_file in missing_bins:
+                    ctrl_links = np.load(os.path.join(ctrl_path, f'ctrl_links_{BIN_CONFIG}', ctrl_file))[:, :n_ctrl]
+
+                    atac_data = adata_atac.layers['counts'][:, ctrl_links[:, 0]]
+                    rna_data = adata_rna.layers['counts'][:, ctrl_links[:, 1]]
+                    _, ctrl_poissonb = ctar.method.vectorized_poisson_regression_final(atac_data, rna_data)
+                    
+                    print(f"# Saving poissonb_{os.path.basename(ctrl_file)} to {final_corr_path}")
+                    np.save(os.path.join(final_corr_path, f'poissonb_{os.path.basename(ctrl_file)}'), ctrl_poissonb.flatten())
 
 
 
@@ -289,6 +392,8 @@ if __name__ == "__main__":
     )
     parser.add_argument("--binning_type", type=str, required=False, default=None)
     parser.add_argument("--pybedtools_path", type=str, required=False, default=None)
+    parser.add_argument("--method", type=str, required=True, default=None,
+        help="pr or corr")
 
     args = parser.parse_args()
     main(args)
---
+++ experiments/job.poisscoeff/poissonb.sh
@@ -0,0 +1,73 @@
+#!/bin/sh
+
+# FILE: /projects/zhanglab/users/ana/cts_peak_gene/experiments/job.poisscoeff/poissonb.sh
+
+MULTIOME_FILE=/projects/zhanglab/users/ana/multiome/processed/neurips_bmmc/bmmc.h5mu
+LINKS_FILE=/projects/zhanglab/users/ana/multiome/simulations/bin_analysis/abc_eval_df.csv
+TARGET_PATH=/projects/zhanglab/users/ana/multiome/simulations/bin_analysis/
+GENOME_FILE=/projects/zhanglab/users/ana/bedtools2/ana_bedfiles/ref/GRCh38.p14.genome.fa.bgz
+BIN_CONFIG='1.1.1.1.10000'
+BIN_TYPE='mean_var'
+PYBEDTOOLS_PATH=/projects/zhanglab/users/ana/bedtools2/bin
+BATCH_SIZE=100
+
+echo "Beginning $BIN_CONFIG create_ctrl job..."
+# Submit create_ctrl and capture job ID
+sbatch -p mzhang,pool1 -t 1-00:00:00 --mem=100Gb -o /home/asprieto/logs/create_ctrl_%J.err -J "ctrl_$BIN_CONFIG" --wait --wrap " \
+source ~/.bashrc && \
+conda activate ctar && \
+python /projects/zhanglab/users/ana/cts_peak_gene/CLI_ctar.py \
+    --job create_ctrl \
+    --multiome_file $MULTIOME_FILE \
+    --links_file $LINKS_FILE \
+    --target_path $TARGET_PATH \
+    --genome_file $GENOME_FILE \
+    --binning_config $BIN_CONFIG \
+    --binning_type $BIN_TYPE \
+    --pybedtools_path $PYBEDTOOLS_PATH \
+    --method pr" 
+
+# Count control link files
+TOTAL_NUM_BIN=$(find "$TARGET_PATH/ctrl_peaks/ctrl_links_$BIN_CONFIG" -type f | wc -l)
+
+# Check if control files < batch size or requires link-based chunks
+if [[ $BIN_CONFIG =~ "inf" ]]; then 
+  NUM_LINKS=$(wc -l < "$LINKS_FILE")
+  TOTAL_NUM_BATCHES=$(( (NUM_LINKS + BATCH_SIZE - 1) / BATCH_SIZE ))
+  if [ "$TOTAL_NUM_BATCHES" -eq 0 ]; then
+    ARRAY_OPT="--array=0"
+  else
+    ARRAY_OPT="--array=0-$((TOTAL_NUM_BATCHES - 1))%20"
+  fi
+else
+  if [ "$TOTAL_NUM_BIN" -lt "$BATCH_SIZE" ]; then
+    TOTAL_NUM_BATCHES=1
+    ARRAY_OPT="--array=0"
+  else
+    TOTAL_NUM_BATCHES=$(( (TOTAL_NUM_BIN + BATCH_SIZE - 1) / BATCH_SIZE ))
+    if [ "$TOTAL_NUM_BATCHES" -eq 0 ]; then
+      ARRAY_OPT="--array=0"
+    else
+      ARRAY_OPT="--array=0-$((TOTAL_NUM_BATCHES - 1))%20"
+    fi
+  fi
+fi
+
+echo "Found $TOTAL_NUM_BIN control files. Submitting $TOTAL_NUM_BATCHES batch job(s)."
+
+echo "Submitting $BIN_CONFIG compute_pr job..."
+# Submit compute_corr job
+sbatch -p mzhang,pool1 -t 1-00:00:00 -x compute-1-1 --mem=24Gb $ARRAY_OPT --mail-type=END --mail-user=asprieto@andrew.cmu.edu \
+  -o /home/asprieto/logs/compute_pr_%A_%a.err -J "corr_$BIN_CONFIG" --wrap " \
+  source ~/.bashrc && \
+  conda activate ctar && \
+  python /projects/zhanglab/users/ana/cts_peak_gene/CLI_ctar.py \
+      --job compute_pr \
+      --multiome_file $MULTIOME_FILE \
+      --batch_size $BATCH_SIZE \
+      --batch_id \$SLURM_ARRAY_TASK_ID \
+      --links_file $LINKS_FILE \
+      --target_path $TARGET_PATH \
+      --binning_config $BIN_CONFIG \
+      --binning_type $BIN_TYPE \
+      --pybedtools_path $PYBEDTOOLS_PATH"
