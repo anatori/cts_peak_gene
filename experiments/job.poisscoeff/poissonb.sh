a5088d2dfc84250e4e7eba774fea97dff81a757f
Anatori Serena Prieto <asprieto@lanec1.compbio.cs.cmu.edu>
2025-08-05 12:09:22 -0400

    cis pairs computation call

--- experiments/job.poisscoeff/poissonb.sh
+++ experiments/job.poisscoeff/poissonb.sh
@@ -9,11 +9,12 @@ GENOME_FILE=/projects/zhanglab/users/ana/bedtools2/ana_bedfiles/ref/GRCh38.p14.g
 BIN_CONFIG=$1
 BIN_TYPE='mean_var'
 PYBEDTOOLS_PATH=/projects/zhanglab/users/ana/bedtools2/bin
-BATCH_SIZE=200
+BATCH_SIZE=100
+
 
 # echo "Beginning $BIN_CONFIG create_ctrl job..."
 # # Submit create_ctrl job
-# sbatch -p mzhang,pool1 -t 1-00:00:00 --mem=82Gb -o /home/asprieto/logs/create_ctrl_%J.err -J "ctrl_$BIN_CONFIG" --wrap " \
+# sbatch -p mzhang,pool1 -t 1-00:00:00 --mem=82Gb -o /projects/zhanglab/users/ana/logs/create_ctrl_%J.err -J "ctrl_$BIN_CONFIG" --wrap " \
 # source ~/.bashrc && \
 # conda activate ctar && \
 # python /projects/zhanglab/users/ana/cts_peak_gene/CLI_ctar.py \
@@ -26,6 +27,22 @@ BATCH_SIZE=200
 #     --binning_type $BIN_TYPE \
 #     --pybedtools_path $PYBEDTOOLS_PATH \
 #     --method pr" 
+
+
+echo "Beginning compute_cis_pairs job..."
+# Submit create_ctrl job
+sbatch -p mzhang,pool1 -t 1-00:00:00 -x compute-1-1 --mem-per-cpu 4Gb -n 1 -c 16 \
+  -o /projects/zhanglab/users/ana/logs/compute_cis_pairs_%J.err -J "pr_cis_pairs" --wrap " \
+  source ~/.bashrc && \
+  conda activate ctar && \
+  python /projects/zhanglab/users/ana/cts_peak_gene/CLI_ctar.py \
+      --job compute_cis_pairs \
+      --multiome_file $MULTIOME_FILE \
+      --links_file $LINKS_FILE \
+      --pybedtools_path $PYBEDTOOLS_PATH \
+      --batch_size $BATCH_SIZE \
+      --method pr" 
+
 
 # # Count control link files
 # TOTAL_NUM_BIN=$(find "$TARGET_PATH/ctrl_peaks/ctrl_links_$BIN_CONFIG" -type f | wc -l)
@@ -53,29 +70,29 @@ BATCH_SIZE=200
 #   fi
 # fi
 
-echo "Found $TOTAL_NUM_BIN control files. Submitting $TOTAL_NUM_BATCHES batch job(s)."
+# echo "Found $TOTAL_NUM_BIN control files. Submitting $TOTAL_NUM_BATCHES batch job(s)."
 
-echo "Submitting $BIN_CONFIG compute_pr job..."
-# Submit compute_corr job
-sbatch -p mzhang,pool1 -t 1-00:00:00 -x compute-1-1 --mem-per-cpu 4Gb -n 1 -c 16 --array 0 --mail-type=END --mail-user=asprieto@andrew.cmu.edu \
-  -o /home/asprieto/logs/compute_pr_%A_%a.err -J "pr_$BIN_CONFIG" --wrap " \
-  source ~/.bashrc && \
-  conda activate ctar && \
-  python /projects/zhanglab/users/ana/cts_peak_gene/CLI_ctar.py \
-      --job compute_pr \
-      --multiome_file $MULTIOME_FILE \
-      --batch_size $BATCH_SIZE \
-      --batch_id \$SLURM_ARRAY_TASK_ID \
-      --links_file $LINKS_FILE \
-      --target_path $TARGET_PATH \
-      --binning_config $BIN_CONFIG \
-      --binning_type $BIN_TYPE \
-      --pybedtools_path $PYBEDTOOLS_PATH"
+# echo "Submitting $BIN_CONFIG compute_pr job..."
+# # Submit compute_corr job
+# sbatch -p mzhang,pool1 -t 1-00:00:00 -x compute-1-1 --mem-per-cpu 4Gb -n 1 -c 16 --array 0 --mail-type=END --mail-user=asprieto@andrew.cmu.edu \
+#   -o /projects/zhanglab/users/ana/logs/compute_pr_%A_%a.err -J "pr_$BIN_CONFIG" --wrap " \
+#   source ~/.bashrc && \
+#   conda activate ctar && \
+#   python /projects/zhanglab/users/ana/cts_peak_gene/CLI_ctar.py \
+#       --job compute_pr \
+#       --multiome_file $MULTIOME_FILE \
+#       --batch_size $BATCH_SIZE \
+#       --batch_id \$SLURM_ARRAY_TASK_ID \
+#       --links_file $LINKS_FILE \
+#       --target_path $TARGET_PATH \
+#       --binning_config $BIN_CONFIG \
+#       --binning_type $BIN_TYPE \
+#       --pybedtools_path $PYBEDTOOLS_PATH"
 
 
 # echo "Beginning $BIN_CONFIG compute_pval job..."
 # # Submit compute_pval job
-# sbatch -p mzhang,pool1 -t 1-00:00:00 --mem=82Gb -o /home/asprieto/logs/compute_pval_%J.err -J "pval_$BIN_CONFIG" --wrap " \
+# sbatch -p mzhang,pool1 -t 1-00:00:00 --mem=82Gb -o /projects/zhanglab/users/ana/logs/compute_pval_%J.err -J "pval_$BIN_CONFIG" --wrap " \
 # source ~/.bashrc && \
 # conda activate ctar && \
 # python /projects/zhanglab/users/ana/cts_peak_gene/CLI_ctar.py \
