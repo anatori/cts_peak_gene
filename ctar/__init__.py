1a51075ee83ac615b1d2ea80c0b682e56dc84bbd
Anatori Serena Prieto <asprieto@lanec1.compbio.cs.cmu.edu>
2026-02-13 07:36:28 -0500

    archive numba.py

--- ctar/__init__.py
+++ ctar/__init__.py
@@ -1,8 +1,8 @@
-from . import resid, simu, numba, method, plot, data_loader, util, chromatin_potential, gentruth, mz, ray, parallel
+from . import resid, simu, method, plot, data_loader, util, chromatin_potential, gentruth, mz, ray, parallel
 
 # expose functions so that scdrs.score_cell, scdrs.preprocess can be called
 # from .method import score_cell
 # from .pp import preprocess
 from .version import __version__,__version_info__
 
-__all__ = ["resid", "simu", "numba", "method", "plot", "data_loader", "util", "chromatin_potential", "gentruth", "mz", "ray", "parallel"]
+__all__ = ["resid", "simu", "method", "plot", "data_loader", "util", "chromatin_potential", "gentruth", "mz", "ray", "parallel"]
