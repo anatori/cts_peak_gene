bdcb6e1a73308b59d94fa106290ece332d86238d
Anatori Serena Prieto <asprieto@lanec1.compbio.cs.cmu.edu>
2026-02-13 07:54:10 -0500

    merge behavior

--- experiments/job.evaluation/add_new_overlap.py
+++ experiments/job.evaluation/add_new_overlap.py
@@ -21,20 +21,21 @@ def main(args):
         if os.path.isdir(f'{AGG_PATH}/{file}'):
             continue
         filename = file.removesuffix('.csv')
-        eval_df_dict[filename] = pd.read_csv(f'{AGG_PATH}/{file}',index_col=0)
+        eval_df_dict[filename] = pd.read_csv(f'{AGG_PATH}/{file}')
         print(filename, eval_df_dict[filename].shape)
         eval_df_dict[filename] = eval_df_dict[filename].merge(new_df[[NEW_COL,'peak','gene']],how='left')
         print(filename, eval_df_dict[filename].shape)
+        print(eval_df_dict[filename].head())
         eval_df_dict[filename].to_csv(f'{AGG_PATH}/{file}',index=False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--agg_path", type=str, default='/projects/zhanglab/users/ana/multiome/validation/pbmc')
-    parser.add_argument("--new_file", type=str, default='/projects/zhanglab/users/ana/multiome/results/ctar/final_eval/pbmc/pbmc_filtered_5.5.5.5.10000/cis_links_df.csv')
-    parser.add_argument("--original_col", type=str, default='5.5.5.5.10000_mcpval')
-    parser.add_argument("--new_col", type=str, default='ctar_filt_10k')
+    parser.add_argument("--agg_path", type=str, default='/projects/zhanglab/users/ana/multiome/validation/neat')
+    parser.add_argument("--new_file", type=str, default='/projects/zhanglab/users/ana/multiome/results/scmultimap/scmm_ctrl/res_df/scmultimap_neat.csv')
+    parser.add_argument("--original_col", type=str, default='scmm_mcpval')
+    parser.add_argument("--new_col", type=str, default='scmm_mc')
     parser.add_argument("--dataset_name", type=str, default='neat')
 
     args = parser.parse_args()
